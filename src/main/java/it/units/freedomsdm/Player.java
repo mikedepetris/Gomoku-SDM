eff0a063ecb0fb59cb1cd026519e0e760ca7f9fd
Biagio Licari <biagiolicari7@gmail.com>
2022-11-17 21:25:10 +0100

    Implemented Base Class Player and tests #5

--- src/main/java/it/units/freedomsdm/Player.java
+++ src/main/java/it/units/freedomsdm/Player.java
@@ -1,4 +1,40 @@
 package it.units.freedomsdm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    private String username;
+    private Colour colour;
+    private final List<Stone> movesList = new ArrayList<>();
+
+    public Player(String username, Colour colour) {
+        this.username = username;
+        this.colour = colour;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Colour getColour() {
+        return colour;
+    }
+
+    public void setColour(Colour colour) {
+        this.colour = colour;
+    }
+
+    public List<Stone> getMovesList() {
+        return movesList;
+    }
+
+    public void addMove(Coordinates coordinates){
+        this.movesList.add(new Stone(coordinates, this.colour));
+    }
+
 }
---
+++ src/test/java/it/units/freedomsdm/PlayerTest.java
@@ -0,0 +1,39 @@
+package it.units.freedomsdm;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+
+    @Test
+    public void testPlayerCreation(){
+        Player player = new Player("xyz", Colour.BLACK);
+        Assertions.assertInstanceOf(Player.class, player);
+    }
+
+    @Test
+    public void testChangeUsernameOfPlayer(){
+        Player player = new Player("xyz", Colour.BLACK);
+        player.setUsername("qwerty");
+        Assertions.assertEquals("qwerty", player.getUsername());
+    }
+
+    @Test
+    public void test2PlayersCreationAreDifferent(){
+        Player player1 = new Player("xyz", Colour.BLACK);
+        Player player2 = new Player("xyz", Colour.WHITE);
+        Assertions.assertNotEquals(player1, player2);
+    }
+
+    @Test
+    public void testGetPlayerColour(){
+        Player player = new Player("xyz", Colour.BLACK);
+        Assertions.assertEquals(Colour.BLACK, player.getColour());
+    }
+
+    @Test
+    public void testAddMoveOfStoneDoneByPlayer(){
+        Player player = new Player("xyz", Colour.BLACK);
+        player.addMove(new Coordinates(0,0));
+        Assertions.assertEquals(1, player.getMovesList().size());
+    }
+}
