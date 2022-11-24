f554e18fb4a9ef2af0579a87ed5e353383aa88fd
marta <marta.tosolini@gmail.com>
2022-11-24 20:18:24 +0100

    Inserito il metodo che permette di stampare la Board nella maniera più semplice, quindi verificando le posizioni della matrice di int per capire se è stata inserita nella Board una pedina bianca, nera o se non è presente una pedina. Notare che ho implementato il tutto nella maniera più basica possibile, dando spazio a possibile refactoring in futuro.

--- src/main/java/it/units/gomokusdm/Board.java
+++ src/main/java/it/units/gomokusdm/Board.java
@@ -4,12 +4,29 @@ public class Board {
     private static final int BOARD_DIMENSION = 19;
     private final int[][] board;
 
+    private static final int EMPTY = 0;
+    private static final int BLACK_STONE = 1;
+    private static final int WHITE_STONE = 2;
+
     public Board() {
         this.board = new int[BOARD_DIMENSION][BOARD_DIMENSION];
     }
 
     public int[][] getBoard() {
         return board;
+    }
+
+    public void printBoard() {
+        for (int i = 0; i<BOARD_DIMENSION; i++) {
+            for (int j = 0; j<BOARD_DIMENSION; j++) {
+                switch (board[i][j]) {
+                    case EMPTY -> System.out.print("|0");
+                    case BLACK_STONE -> System.out.print("|B");
+                    case WHITE_STONE -> System.out.print("|W");
+                }
+            }
+        }
+
     }
 
 
