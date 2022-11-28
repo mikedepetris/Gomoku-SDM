e749ee248cfd60eaee5ca612cd4b0746521db65f
marta <marta.tosolini@gmail.com>
2022-11-28 11:47:15 +0100

    Il giocatore nero, nella prima mossa, è obbligato a inserire la pedina nera al centro. Come possiamo fare? - Impostiamo la configurazione iniziale con la pedina nera centrale già inserita - Impostiamo un check sulla prima mossa del nero, che deve essere per forza al centro

--- src/test/java/it/units/gomokusdm/GameTest.java
+++ src/test/java/it/units/gomokusdm/GameTest.java
@@ -17,13 +17,16 @@ public class GameTest {
     }
 
     // Va prima implementato setCell in Board
+    // Il test è invalido perchè il giocatore che inizia ("First"), è obbligato a inserire la pedina nera al centro.
+    // Non può inserire nelle coordinate (1,1), ma mi aspetto in questo caso le coordinate (10,10) come prima mossa
+    // Le successive mosse invece sono casuali
     @Test
     public void testMakeFirstMove() {
         Player firstPlayer = new Player("First", Colour.BLACK);
         Player secondPlayer = new Player("Second", Colour.WHITE);
         Board board = new Board();
         Game game = new Game(board, firstPlayer, secondPlayer);
-        game.makeMove(firstPlayer, new Coordinates(1, 1));
+        game.makeMove(firstPlayer, new Coordinates(1, 1)); // deve essere 10,10
         Colour stoneColor = switch (board.getBoard()[1][1]) {
             case 1 -> Colour.BLACK;
             case 2 -> Colour.WHITE;
@@ -33,13 +36,16 @@ public class GameTest {
     }
 
     // Va prima implementato setCell in Board
+    // Il test è invalido perchè il giocatore che inizia ("First"), è obbligato a inserire la pedina nera al centro.
+    // Non può inserire nelle coordinate (1,1), ma mi aspetto in questo caso le coordinate (10,10) come prima mossa
+    // Le successive mosse invece sono casuali
     @Test
     public void testMakeConsecutiveMoves() {
         Player firstPlayer = new Player("First", Colour.BLACK);
         Player secondPlayer = new Player("Second", Colour.WHITE);
         Board board = new Board();
         Game game = new Game(board, firstPlayer, secondPlayer);
-        game.makeMove(firstPlayer, new Coordinates(1, 1));
+        game.makeMove(firstPlayer, new Coordinates(1, 1)); // deve essere 10,10
         game.makeMove(secondPlayer, new Coordinates(1, 2));
         game.makeMove(firstPlayer, new Coordinates(1, 3));
         int[][] expectedBoard =
