f37e1baa01048193e0ad4d8fe8b2dbca79f921c9
marta <marta.tosolini@gmail.com>
2022-12-04 18:10:58 +0100

    Aggiornato (intanto) il metodo getAdjacentCoordinates() che trova qualsiasi coordinata adiacente (anche negativa o che non rispetti la size) nella classe Coordinates, aggiornato quindi il metodo isThereAnAdjacentStone() nella classe Game. Ridotta la complessità di checkIfThereAreFiveConsecutiveStones() con l'aggiunta di un while. Messo un test nella classe Game che verifica la vittoria dopo ogni mossa.

--- src/main/java/it/units/gomokusdm/Coordinates.java
+++ src/main/java/it/units/gomokusdm/Coordinates.java
@@ -33,17 +33,13 @@ public class Coordinates {
         this.colIndex = colIndex;
     }
 
-    // siamo sicuri di voler mettere questo metodo qui? per trovare le coordinate adiacenti ci serve la dimensione
-    // della board per soddisfare la condizione che le coordinate siano < 19 (in questo caso)
-    // forse meglio board.getAdjacentCoordinates(coordinata/cella) così abbiamo già la dimensione della board?
     public ArrayList<Coordinates> getAdjacentCoordinates() {
         ArrayList<Coordinates> adjacentCoordinates = new ArrayList<>();
         for (int row = -1; row <= 1; row++) {
             for (int col = -1; col <= 1; col++) {
                 int rowCoordinate = rowIndex + row;
                 int columnCoordinate = colIndex + col;
-                if ((rowCoordinate >= 0 && rowCoordinate < 19) && (columnCoordinate >= 0 && columnCoordinate < 19))
-                    adjacentCoordinates.add(new Coordinates(rowCoordinate, columnCoordinate));
+                adjacentCoordinates.add(new Coordinates(rowCoordinate, columnCoordinate));
             }
         }
         return adjacentCoordinates;
