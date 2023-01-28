e4faef8b6c6554dcc20c63a611557ad561ff88ef
Federico Calandra <federico12597@gmail.com>
2023-01-28 16:43:12 +0100

    Refactoring classe BoardFormatter

--- src/main/java/it/units/gomokusdm/BoardFormatter.java
+++ src/main/java/it/units/gomokusdm/BoardFormatter.java
@@ -7,34 +7,34 @@ import java.util.stream.IntStream;
 public class BoardFormatter {
 
     public static String formatBoard(Board board) {
-        StringBuilder tmp = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
         String repeatedLine = "|\t".repeat(board.getBoardDimension());
         List<List<String>> boardPartitionString = Utilities.partition(Arrays.stream(board.toString().split("")).toList(), board.getBoardDimension());
-        tmp.append(System.lineSeparator());
+        stringBuilder.append(System.lineSeparator());
         for (int row = 0; row < boardPartitionString.size(); row++) {
-            tmp.append(String.format("%1s", row)).append("\t");
+            stringBuilder.append(String.format("%1s", row)).append("\t");
             for (int stone = 0; stone < boardPartitionString.get(row).size(); stone++) {
                 if (stone == boardPartitionString.get(row).size() - 1) {
-                    tmp.append(String.format("%-4s", boardPartitionString.get(row).get(stone)))
+                    stringBuilder.append(String.format("%-4s", boardPartitionString.get(row).get(stone)))
                             .append(System.lineSeparator());
                 } else {
-                    tmp.append(String.format("%-4s", boardPartitionString.get(row).get(stone))
+                    stringBuilder.append(String.format("%-4s", boardPartitionString.get(row).get(stone))
                             .replace(" ", "-"));
                 }
             }
             if (!(row == boardPartitionString.size() - 1)) {
-                tmp.append("\t")
+                stringBuilder.append("\t")
                         .append(repeatedLine)
                         .append(System.lineSeparator());
             }
         }
-        tmp.append("\t");
+        stringBuilder.append("\t");
         IntStream.range(0, board.getBoardDimension())
                 .forEach(value ->
-                        tmp.append(String.format("%1s", value)).append("\t"));
-        tmp.append(System.lineSeparator());
+                        stringBuilder.append(String.format("%1s", value)).append("\t"));
+        stringBuilder.append(System.lineSeparator());
 
-        return tmp.toString();
+        return stringBuilder.toString();
     }
 
 }
