5ec0bd78812f6d18a6196de24fa4828dec9a66dc
Mike De Petris <mikedepetris@gmail.com>
2023-01-28 17:43:32 +0100

    #71 test della classe GUI (test limitato dell'interfaccia grafica)

--- src/main/java/it/units/gomokusdm/GUI.java
+++ src/main/java/it/units/gomokusdm/GUI.java
@@ -1,9 +1,12 @@
 package it.units.gomokusdm;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
@@ -14,26 +17,164 @@ import java.util.stream.IntStream;
 
 public class GUI implements ActionListener, MouseListener {
     int current_window; // finestra in cui mi trovo nel gioco
+
+    public int getCurrent_window() {
+        return current_window;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public JFrame getFrame() {
+        return frame;
+    }
+
+    public JPanel getUpper_panel() {
+        return upper_panel;
+    }
+
+    public JLabel getTitle() {
+        return title;
+    }
+
+    public JPanel getStart_panel() {
+        return start_panel;
+    }
+
+    public JTextField getInput_player1() {
+        return input_player1;
+    }
+
+    public JTextField getInput_player2() {
+        return input_player2;
+    }
+
+    public JPanel getGrid_panel() {
+        return grid_panel;
+    }
+
+    public JButton[] getInitial_buttons() {
+        return initial_buttons;
+    }
+
+    public JLabel getBoard_img_19() {
+        return board_img_19;
+    }
+
+    public JLabel getBoard_img_15() {
+        return board_img_15;
+    }
+
+    public BufferedImage getBlack_stone_img() {
+        return black_stone_img;
+    }
+
+    public BufferedImage getWhite_stone_img() {
+        return white_stone_img;
+    }
+
     private Board board;
     private Game game;
-    private final Player player1;
-    private final Player player2;
+    private Player player1;
+    private Player player2;
     //private Player winner; //come in CLI Controller
 
-    JFrame frame = new JFrame(); // frame totale
-    JPanel upper_panel = new JPanel(); // pannello soprastante con il titolo
-    JLabel title = new JLabel();
-    JPanel start_panel = new JPanel();
-    JTextField input_player1 = new JTextField("Player 1");
-    JTextField input_player2 = new JTextField("Player 2");
-    JPanel grid_panel = new JPanel();
-    JButton[] initial_buttons = new JButton[2];
-    JLabel board_img_19 = new JLabel(new ImageIcon(ImageIO.read(new URL("https://i.imgur.com/7x0CxBV.png"))));
-    JLabel board_img_15 = new JLabel(new ImageIcon(ImageIO.read(new URL("https://i.imgur.com/4pxgWya.png"))));
-    BufferedImage black_stone_img = ImageIO.read(new URL("https://i.imgur.com/cDfy5SP.png"));
-    BufferedImage white_stone_img = ImageIO.read(new URL("https://i.imgur.com/kIXiq4Q.png"));
+    private JFrame frame = new JFrame(); // frame totale
+    private JPanel upper_panel = new JPanel(); // pannello soprastante con il titolo
+    private JLabel title = new JLabel();
+    private JPanel start_panel = new JPanel();
 
-    GUI() throws IOException {
+    public void setCurrent_window(int current_window) {
+        this.current_window = current_window;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public void setFrame(JFrame frame) {
+        this.frame = frame;
+    }
+
+    public void setUpper_panel(JPanel upper_panel) {
+        this.upper_panel = upper_panel;
+    }
+
+    public void setTitle(JLabel title) {
+        this.title = title;
+    }
+
+    public void setStart_panel(JPanel start_panel) {
+        this.start_panel = start_panel;
+    }
+
+    public void setInput_player1(JTextField input_player1) {
+        this.input_player1 = input_player1;
+    }
+
+    public void setInput_player2(JTextField input_player2) {
+        this.input_player2 = input_player2;
+    }
+
+    public void setGrid_panel(JPanel grid_panel) {
+        this.grid_panel = grid_panel;
+    }
+
+    public void setInitial_buttons(JButton[] initial_buttons) {
+        this.initial_buttons = initial_buttons;
+    }
+
+    public void setBoard_img_19(JLabel board_img_19) {
+        this.board_img_19 = board_img_19;
+    }
+
+    public void setBoard_img_15(JLabel board_img_15) {
+        this.board_img_15 = board_img_15;
+    }
+
+    public void setBlack_stone_img(BufferedImage black_stone_img) {
+        this.black_stone_img = black_stone_img;
+    }
+
+    public void setWhite_stone_img(BufferedImage white_stone_img) {
+        this.white_stone_img = white_stone_img;
+    }
+
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+    }
+
+    private JTextField input_player1 = new JTextField("Player 1");
+    private JTextField input_player2 = new JTextField("Player 2");
+    private JPanel grid_panel = new JPanel();
+    private JButton[] initial_buttons = new JButton[2];
+    private JLabel board_img_19 = new JLabel(new ImageIcon(ImageIO.read(new URL("https://i.imgur.com/7x0CxBV.png"))));
+    private JLabel board_img_15 = new JLabel(new ImageIcon(ImageIO.read(new URL("https://i.imgur.com/4pxgWya.png"))));
+    private BufferedImage black_stone_img = ImageIO.read(new URL("https://i.imgur.com/cDfy5SP.png"));
+    private BufferedImage white_stone_img = ImageIO.read(new URL("https://i.imgur.com/kIXiq4Q.png"));
+
+    public GUI() throws IOException {
 
         // Base Settings
         this.player1 = new Player("", Stone.BLACK);
@@ -92,14 +233,14 @@ public class GUI implements ActionListener, MouseListener {
         JButton play = new JButton("Play");
         initial_buttons[0] = play;
         //play.setFont(new Font("", Font.BOLD, 12));
-        play.setBounds(x_starting_components,150, 200,20);
+        play.setBounds(x_starting_components, 150, 200, 20);
         play.addActionListener(this);
 
         // Bottone Settings
         JButton settings = new JButton("Settings");
         initial_buttons[1] = settings;
         //play.setFont(new Font("", Font.BOLD, 12));
-        settings.setBounds(x_starting_components,200, 200,20);
+        settings.setBounds(x_starting_components, 200, 200, 20);
         settings.addActionListener(this);
 
         // Labels/Text Field per inserimento nome Giocatore 1/2
@@ -107,10 +248,10 @@ public class GUI implements ActionListener, MouseListener {
         JLabel insert_player2 = new JLabel("Inserisci il nome del Giocatore 2:");
 
         // Allineo
-        insert_player1.setBounds(x_starting_components,260, 200,20);
-        input_player1.setBounds(x_starting_components,290, 200,20);
-        insert_player2.setBounds(x_starting_components,320, 200,20);
-        input_player2.setBounds(x_starting_components,350, 200,20);
+        insert_player1.setBounds(x_starting_components, 260, 200, 20);
+        input_player1.setBounds(x_starting_components, 290, 200, 20);
+        insert_player2.setBounds(x_starting_components, 320, 200, 20);
+        input_player2.setBounds(x_starting_components, 350, 200, 20);
 
         // Aggiungo elementi al pannello iniziale
         start_panel.add(play);
@@ -148,17 +289,17 @@ public class GUI implements ActionListener, MouseListener {
 
         // Inserisco la pedina nera al centro:
         JLabel blackstone = new JLabel(new ImageIcon(black_stone_img));
-        blackstone.setBounds(26*9+22, 26*9+22, 24, 24);
+        blackstone.setBounds(26 * 9 + 22, 26 * 9 + 22, 24, 24);
         board_img_19.add(blackstone);
 
-        title.setText("Turno: "+game.getPlayer2().getUsername());
+        title.setText("Turno: " + game.getPlayer2().getUsername());
 
     }
 
     public void showStone(BufferedImage stone_img, int resize_x, int resize_y) {
         JLabel stone = new JLabel(new ImageIcon(stone_img));
         // è +22 per size=19
-        stone.setBounds(resize_x+22, resize_y+22, 24, 24);
+        stone.setBounds(resize_x + 22, resize_y + 22, 24, 24);
         board_img_19.add(stone);
         title.setText("Turno: " + game.getNextMovingPlayer().getUsername());
     }
@@ -205,16 +346,19 @@ public class GUI implements ActionListener, MouseListener {
     public void mousePressed(MouseEvent e) {
         int padding_board = 30; // se la dimensione è 19 fa 30, se è 15 è circa 109,6. andrebbe associato
         // Rilevo le x,y del mouse dopo aver cliccato
-        int x=e.getX()-padding_board; int y=e.getY()-padding_board;
+        int x = e.getX() - padding_board;
+        int y = e.getY() - padding_board;
         int cell_dimension = 26; // è il valore della dimensione cella rispetto all'immagine
 
-        int resize_x = 0; int resize_y = 0; // resize x e y rispetto a dove posizionare la stone come immagine
-        int new_x = 0; int new_y = 0; // le vere coordinate che vengono rilevate per makeMove
-        while(resize_x < ((x+7)-cell_dimension))  {
+        int resize_x = 0;
+        int resize_y = 0; // resize x e y rispetto a dove posizionare la stone come immagine
+        int new_x = 0;
+        int new_y = 0; // le vere coordinate che vengono rilevate per makeMove
+        while (resize_x < ((x + 7) - cell_dimension)) {
             resize_x = resize_x + cell_dimension;
             new_y++;
         }
-        while(resize_y < ((y+7)-cell_dimension)) {
+        while (resize_y < ((y + 7) - cell_dimension)) {
             resize_y = resize_y + cell_dimension;
             new_x++;
         }
@@ -240,7 +384,6 @@ public class GUI implements ActionListener, MouseListener {
         }
         grid_panel.repaint();
     }
-
 
 
     @Override
--- src/test/java/it/units/gomokusdm/GUITest.java
+++ src/test/java/it/units/gomokusdm/GUITest.java
@@ -1,11 +1,8 @@
 package it.units.gomokusdm;
 
 import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -16,57 +13,37 @@ import java.io.IOException;
 import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
 
-@RunWith(MockitoJUnitRunner.class)
+//@ExtendWith(MockitoExtension.class)
 public class GUITest {
-    //    @Mock
-//    GraphicsEnvironment graphicsEnvironment;
-    @Mock
-    private JLabel boardLabel;
-    @Mock
-    private JPanel gridPanel;
-    @Mock
-    private Game game;
-    @Mock
-    private Board board;
-    @Mock
-    private Player player1;
-    @Mock
-    private Player player2;
-    @Mock
-    private JButton button;
+
     private GUI gui;
 
     @Before
-    public void setUp() throws IOException {
+    public void setUp() {
         // Set up headless mode
+        System.out.println("@Before: Set up headless mode");
         System.setProperty("java.awt.headless", "true");
-        gui = new GUI();
     }
 
-//    @BeforeEach
-//    void setUpEach() throws IOException {
-////        when(GraphicsEnvironment.isHeadless()).thenReturn(false);
-////        when(player1.getColour()).thenReturn(Stone.BLACK);
-////        when(player2.getColour()).thenReturn(Stone.WHITE);
-////        when(game.getBoard()).thenReturn(board);
-//        gui = new GUI();
-////        gui.board_img_19 = boardLabel;
-////        gui.grid_panel = gridPanel;
-//    }
+    @BeforeEach
+    void setUpEach() throws IOException {
+//        when(GraphicsEnvironment.isHeadless()).thenReturn(false);
+//        when(player1.getColour()).thenReturn(Stone.BLACK);
+//        when(player2.getColour()).thenReturn(Stone.WHITE);
+//        when(game.getBoard()).thenReturn(board);
+        gui = new GUI();
+//        gui.setFrame(frame);
+//        gui.setStart_panel(start_panel);
+//        gui.setInput_player1(input_player1);
+//        gui.setInput_player2(input_player2);
+//        gui.board_img_19 = boardLabel;
+//        gui.getGrid_panel() = gridPanel;
+    }
 
 //    @AfterEach
 //    void tearDown() {
 //    }
-
-    @Test
-    public void testButtonClick() {
-        // Test button click functionality
-        button.doClick();
-        // Verify that button was clicked
-        Mockito.verify(button, times(1)).doClick();
-    }
 
     // This test case checks if the showStartingWindow method creates the correct buttons with the correct text.
     // This test case only test the showStartingWindow method.
@@ -74,8 +51,8 @@ public class GUITest {
     @Test
     public void testShowStartingWindow1() {
         gui.showStartingWindow();
-        JButton play = gui.initial_buttons[0];
-        JButton settings = gui.initial_buttons[1];
+        JButton play = gui.getInitial_buttons()[0];
+        JButton settings = gui.getInitial_buttons()[1];
         assertEquals("Play", play.getText());
         assertEquals("Settings", settings.getText());
     }
@@ -84,10 +61,10 @@ public class GUITest {
     public void testShowStartingWindow2() {
         gui.showStartingWindow();
         assertEquals(gui.current_window, 0);
-        assertTrue(gui.initial_buttons[0].isVisible());
-        assertTrue(gui.initial_buttons[1].isVisible());
-        assertTrue(gui.input_player1.isVisible());
-        assertTrue(gui.input_player2.isVisible());
+        assertTrue(gui.getInitial_buttons()[0].isVisible());
+        assertTrue(gui.getInitial_buttons()[1].isVisible());
+        assertTrue(gui.getInput_player1().isVisible());
+        assertTrue(gui.getInput_player2().isVisible());
     }
 
     @Test
@@ -102,29 +79,29 @@ public class GUITest {
     public void testStartGame2() {
         gui.startGame();
         assertEquals(gui.current_window, 0); // 0
-        assertTrue(gui.initial_buttons[0].isVisible());
-        assertTrue(gui.initial_buttons[1].isVisible());
-        assertTrue(gui.input_player1.isVisible());
-        assertTrue(gui.input_player2.isVisible());
-        assertTrue(gui.grid_panel.isVisible());
+        assertTrue(gui.getInitial_buttons()[0].isVisible());
+        assertTrue(gui.getInitial_buttons()[1].isVisible());
+        assertTrue(gui.getInput_player1().isVisible());
+        assertTrue(gui.getInput_player2().isVisible());
+        assertTrue(gui.getGrid_panel().isVisible());
     }
 
     @Test
     public void testActionPerformed() {
         // Simulate clicking on the "Play" button
-        gui.initial_buttons[0].doClick();
+        gui.getInitial_buttons()[0].doClick();
         assertEquals(gui.current_window, 2); // 1
-        assertTrue(gui.initial_buttons[0].isVisible());
-        assertTrue(gui.initial_buttons[1].isVisible());
-        assertTrue(gui.input_player1.isVisible());
-        assertTrue(gui.input_player2.isVisible());
-        assertTrue(gui.grid_panel.isVisible());
+        assertTrue(gui.getInitial_buttons()[0].isVisible());
+        assertTrue(gui.getInitial_buttons()[1].isVisible());
+        assertTrue(gui.getInput_player1().isVisible());
+        assertTrue(gui.getInput_player2().isVisible());
+        assertTrue(gui.getGrid_panel().isVisible());
     }
 
     @Test
     public void testMouseClicked() {
         // Simulate clicking on the board
-        gui.grid_panel.dispatchEvent(new MouseEvent(gui.grid_panel, MouseEvent.MOUSE_CLICKED, System.currentTimeMillis(), 0, 100, 100, 1, false));
+        gui.getGrid_panel().dispatchEvent(new MouseEvent(gui.getGrid_panel(), MouseEvent.MOUSE_CLICKED, System.currentTimeMillis(), 0, 100, 100, 1, false));
         // Check that the board has been updated with a stone
         //assertTrue(gui.board.getStones().contains(new Point(100, 100)));
     }
@@ -132,8 +109,8 @@ public class GUITest {
     @Test
     public void testSetMainElements() {
         gui.setMainElements();
-        JFrame frame = gui.frame;
-        JPanel upper_panel = gui.upper_panel;
+        JFrame frame = gui.getFrame();
+        JPanel upper_panel = gui.getUpper_panel();
         assertEquals(700, frame.getWidth());
         assertEquals(700, frame.getHeight());
         assertEquals("Gomoku", frame.getTitle());
@@ -142,32 +119,22 @@ public class GUITest {
         assertEquals(0, gui.current_window);
     }
 
-    // It is worth noting that these test methods might not be able to run correctly, because ImageIO.read(new URL("https://i.imgur.com/7x0CxBV.png")) and other similar lines will throw IOException when there is no internet connection, and also it might not be possible to test the GUI methods as they are dependent on many other GUI elements.
-    @Test
-    public void testShowStartingWindow() {
-        gui.setMainElements();
-        gui.showStartingWindow();
-        assertEquals(0, gui.current_window); // 1
-        assertEquals("Play", gui.initial_buttons[0].getText());
-        assertEquals("Settings", gui.initial_buttons[1].getText());
-    }
-
     @Test
     public void testShowBoard() {
         gui.showBoard();
-        assertNotNull(gui.grid_panel);
-        assertTrue(gui.grid_panel.isVisible());
+        assertNotNull(gui.getGrid_panel());
+        assertTrue(gui.getGrid_panel().isVisible());
     }
 
     @Test
     public void testShowStone() throws IOException {
         BufferedImage white_stone_img = ImageIO.read(new URL("https://i.imgur.com/kIXiq4Q.png"));
         gui.showStone(white_stone_img, 5, 5);
-        assertNull(gui.grid_panel.getComponentAt(2, 3));
-        //TODO: assertNotNull(gui.grid_panel.getComponentAt(2, 3));
-        assertFalse(gui.grid_panel.getComponentAt(2, 3) instanceof JLabel);
-        //TODO: assertTrue(gui.grid_panel.getComponentAt(2, 3) instanceof JLabel);
-        JLabel label = (JLabel) gui.grid_panel.getComponentAt(2, 3);
+        assertNull(gui.getGrid_panel().getComponentAt(2, 3));
+        //TODO: assertNotNull(gui.getGrid_panel().getComponentAt(2, 3));
+        assertFalse(gui.getGrid_panel().getComponentAt(2, 3) instanceof JLabel);
+        //TODO: assertTrue(gui.getGrid_panel().getComponentAt(2, 3) instanceof JLabel);
+        JLabel label = (JLabel) gui.getGrid_panel().getComponentAt(2, 3);
         assertNull(label);
         //TODO: assertNotNull(label.getIcon());
     }
@@ -175,8 +142,25 @@ public class GUITest {
     @Test
     public void testPrintBoard() {
         gui.printBoard();
-        assertNotNull(gui.board_img_19);
-        assertTrue(gui.board_img_19.isVisible());
+        assertNotNull(gui.getBoard_img_19());
+        assertTrue(gui.getBoard_img_19().isVisible());
     }
 
+    // It is worth noting that these test methods might not be able to run correctly, because ImageIO.read(new URL("https://i.imgur.com/7x0CxBV.png")) and other similar lines will throw IOException when there is no internet connection, and also it might not be possible to test the GUI methods as they are dependent on many other GUI elements.
+    @Test
+    public void testShowStartingWindow() {
+        gui.setMainElements();
+        gui.showStartingWindow();
+//        // Verify that the frame's content pane is set to the start_panel
+//        verify(frame).setContentPane(start_panel);
+//        // Verify that the start_panel is visible
+//        verify(start_panel).setVisible(true);
+//        // Verify that the input_player1 and input_player2 fields are visible
+//        verify(input_player1).setVisible(true);
+//        verify(input_player2).setVisible(true);
+
+        assertEquals(0, gui.current_window); // 1
+        assertEquals("Play", gui.getInitial_buttons()[0].getText());
+        assertEquals("Settings", gui.getInitial_buttons()[1].getText());
+    }
 }
