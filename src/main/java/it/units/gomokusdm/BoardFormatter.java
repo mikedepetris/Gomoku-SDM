9c317eceb172c829fb4cf0cac2ea818bb635fe4b
Mike De Petris <mikedepetris@gmail.com>
2023-02-01 08:51:12 +0100

    righe 19-->1 con test

--- src/main/java/it/units/gomokusdm/BoardFormatter.java
+++ src/main/java/it/units/gomokusdm/BoardFormatter.java
@@ -16,8 +16,48 @@ public class BoardFormatter {
         // linea vuota iniziale
         stringBuilder.append(System.lineSeparator());
         for (int row = 0; row < boardPartitionString.size(); row++) {
-            // numero di riga a sinistra TODO: 19-->1 ma poi cambiano gli inserimenti coordinate
+            // numero di riga a sinistra
             stringBuilder.append(String.format("%1s", row)).append("\t");
+            for (int stone = 0; stone < boardPartitionString.get(row).size(); stone++) {
+                // da sinistra a destra per ogni cella usa "*" o "B" o "W" separati da spazi per QUATTRO posizioni
+                if (stone == boardPartitionString.get(row).size() - 1) {
+                    stringBuilder.append(String.format("%-4s", boardPartitionString.get(row).get(stone)))
+                            .append(System.lineSeparator());
+                } else {
+                    stringBuilder.append(String.format("%-4s", boardPartitionString.get(row).get(stone))
+                            .replace(" ", "-"));
+                }
+            }
+            // tranne l'ultima riga inserisce le indicazioni delle colonne con il simbolo di pipe | separato da TAB
+            if (row != boardPartitionString.size() - 1) {
+                stringBuilder.append("\t")
+                        .append(repeatedLine)
+                        .append(System.lineSeparator());
+            }
+        }
+        // riga finale con i numeri di colonna TODO: a-->o ma poi cambiano gli inserimenti coordinate
+        stringBuilder.append("\t");
+        IntStream.range(0, board.getBoardDimension())
+                .forEach(value ->
+                        stringBuilder.append(String.format("%1s", value)).append("\t"));
+        stringBuilder.append(System.lineSeparator());
+
+        return stringBuilder.toString();
+    }
+
+    public static String formatBoard15to1(Board board) {
+        StringBuilder stringBuilder = new StringBuilder();
+        // indicazioni delle colonne con il simbolo di pipe | separato da TAB
+        String repeatedLine = "|\t".repeat(board.getBoardDimension());
+        // [[*, *, *, *, *, *, *, *, *, *, B, W, *, *, *, *, *, *, *], [*, *, *, *, *, *, ...
+        List<List<String>> boardPartitionString = Utilities.partition(Arrays.stream(board.toString().split(""))
+                .toList(), board.getBoardDimension());
+        // linea vuota iniziale
+        stringBuilder.append(System.lineSeparator());
+        for (int row = 0; row < boardPartitionString.size(); row++) {
+            // numero di riga a sinistra 19-->1
+            // TODO: cambiare gli inserimenti coordinate
+            stringBuilder.append(String.format("%1s", boardPartitionString.size() - row)).append("\t");
             for (int stone = 0; stone < boardPartitionString.get(row).size(); stone++) {
                 // da sinistra a destra per ogni cella usa "*" o "B" o "W" separati da spazi per QUATTRO posizioni
                 if (stone == boardPartitionString.get(row).size() - 1) {
--- src/test/java/it/units/gomokusdm/BoardFormatterTest.java
+++ src/test/java/it/units/gomokusdm/BoardFormatterTest.java
@@ -90,4 +90,44 @@ public class BoardFormatterTest {
         Assertions.assertEquals(expectedOutputString, formattedBoard);
     }
 
+    @Test
+    void testWhiteBoardFormatting15to1() {
+        Board board = new BoardImplementation(BOARD_DIMENSION);
+        String formattedBoard = BoardFormatter.formatBoard15to1(board);
+
+        String expectedOutputString = System.lineSeparator() +
+                "15\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t" + System.lineSeparator() +
+                "14\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t" + System.lineSeparator() +
+                "13\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t" + System.lineSeparator() +
+                "12\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t" + System.lineSeparator() +
+                "11\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t" + System.lineSeparator() +
+                "10\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t" + System.lineSeparator() +
+                "9\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t" + System.lineSeparator() +
+                "8\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t" + System.lineSeparator() +
+                "7\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t" + System.lineSeparator() +
+                "6\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t" + System.lineSeparator() +
+                "5\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t" + System.lineSeparator() +
+                "4\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t" + System.lineSeparator() +
+                "3\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t" + System.lineSeparator() +
+                "2\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t|\t" + System.lineSeparator() +
+                "1\t*---*---*---*---*---*---*---*---*---*---*---*---*---*---*   " + System.lineSeparator() +
+                "\t0\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12\t13\t14\t" +
+                System.lineSeparator();
+
+        Assertions.assertEquals(expectedOutputString, formattedBoard);
+    }
 }
